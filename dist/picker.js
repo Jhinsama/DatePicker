59bd745e532d27d627c9b8c213bd4129fc7eed8b
Jhin <a598805559@hotmail.com>
2018-06-19 17:17:14 +0800

    添加通用时间处理函数

--- dist/picker.js
+++ dist/picker.js
@@ -7,8 +7,19 @@
   const weekArr = ['日','一','二','三','四','五','六']
   const monthArr = ['一','二','三','四','五','六','七','八','九','十','十一','十二']
   const monthEnArr = ['Jan','Fed','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+  const dateKey = ['Y', 'M', 'D', 'h', 'm', 's']
+  const formatKey = ['YYYY', 'MM', 'DD', 'hh', 'mm', 'ss']
+  const multiple = {
+    Y: [31536e6, 316224e5],
+    M: { '28': 24192e5, '29': 25056e5, '30': 2592e6, '31': 26784e5 },
+    W: 6048e5,
+    D: 864e5,
+    h: 36e5,
+    m: 6e4,
+    s: 1e3
+  }
   let defConfig = {
-    format: 'YYYY-MM-DD'
+    format: 'YYYY/MM/DD'
   }
 
   const _ = {
@@ -125,7 +136,7 @@
       }
       return obj
     },
-    assign() {
+    assign() {    // 对象合并
       let arg = arguments
       let obj = {}
       for (let i = 0; i < arg.length; i++) {
@@ -134,20 +145,6 @@
             obj[key] = arg[i][key]
           }
       }
-      return obj
-    },
-    isInput(el) {   // 判断元素是否为输入框
-      if (el && el.nodeType === 1 && el.nodeName.toLowerCase() === 'input') return true
-      return false
-    },
-    formatInfo(str) {   // 判断时间格式所包含的值
-      let info = { str }
-      if (/Y{4}(?!Y)/.test(str)) info.Y = true
-      if (/M{2}(?!M)/.test(str)) info.M = true
-      if (/D{2}(?!D)/.test(str)) info.D = true
-      if (/h{2}(?!h)/.test(str)) info.h = true
-      if (/m{2}(?!m)/.test(str)) info.m = true
-      if (/s{2}(?!s)/.test(str)) info.s = true
       return obj
     },
     sup(num, count) {   // 数字不足指定位数前面补零
@@ -159,15 +156,109 @@
         }
       return num > 9 ? num : before + num
     },
-    dayCount(y, m) {
+    formatInfo(format) {   // 判断时间格式所包含的值
+      let info = { format }
+      if (/Y{4}/.test(format)) info.Y = true
+      if (/M{2}/.test(format)) info.M = true
+      if (/D{2}/.test(format)) info.D = true
+      if (/h{2}/.test(format)) info.h = true
+      if (/m{2}/.test(format)) info.m = true
+      if (/s{2}/.test(format)) info.s = true
+      return obj
+    },
+    dayCount(y, m) {    // 获取指定月份的天数
       if (m != 2) return dayArr[m - 1]
       if (y % 4 > 0) return 28
       return 29
+    },
+    joinDefDate(obj) {    // 拼接默认时间格式
+      return obj.Y + '/' +
+        obj.M + '/' +
+        obj.D + ' ' +
+        obj.h + ':' +
+        obj.m + ':' +
+        obj.s
+    },
+    joinDateObj(obj, format) {    // 拼接时间
+      format = format || defConfig.format
+      return format
+        .replace(/Y{4}/, obj.Y)
+        .replace(/M{2}/, _.sup(obj.M))
+        .replace(/D{2}/, _.sup(obj.D))
+        .replace(/h{2}/, _.sup(obj.h))
+        .replace(/m{2}/, _.sup(obj.m))
+        .replace(/s{2}/, _.sup(obj.s))
+    },
+    resetDate(str, format) {    // 根据时间格式获取自定义时间对象
+      format = format || defConfig.format
+      let obj = {}
+      for (let i = 0; i < 6; i++) {
+        let search = formatKey[i]
+        let index = format.indexOf(search)
+        if (index + 1 > 0) obj[dateKey[i]] = str.substr(index, search.length)
+      }
+      return obj
+    },
+    sortDate(a, b) {    // 时间对比
+      let max, min
+      let _a = new Date(a), _b = new Date(b)
+      if (_a > _b) max = a, min = b
+      else max = b, min = a
+      return {max, min}
+    },
+    getRangeMs(obj, str, minus) {   // 计算时间范围相差的毫秒数
+      let Y = obj.Y, M = obj.M
+      let ms = 0
+      let arr = str.match(/[YMDWhms]\d+/g) || []
+      for (let i = 0; i < arr.length; i++) {
+        let key = arr[i].substr(0, 1)
+        let num = Number(arr[i].substr(1))
+        switch (key) {
+          case 'Y':
+            for (let l = 0; l < num; l++) {
+              if (minus) Y--
+              if (Y % 4) ms += multiple.Y[0]
+              else ms += multiple.Y[1]
+              if (!minus) Y++
+            }
+          break
+          case 'M':
+            for (let l = 0; l < num; l++) {
+              if (minus) {
+                M--
+                if (M < 1) Y--, M = 12
+              }
+              ms += multiple.M[_.dayCount(Y, M)]
+              if (!minus) {
+                M++
+                if (M > 12) Y++, M = 1
+              }
+            }
+          break
+          default:
+            ms += multiple[key] * num
+        }
+      }
+      return ms
+    },
+    newDateObj(date) {    // 获取自定义时间对象
+      date = date || new Date
+      return {
+        date,
+        Y: date.getFullYear(),
+        M: date.getMonth() + 1,
+        D: date.getDate(),
+        W: date.getDay(),
+        h: date.getHours(),
+        m: date.getMinutes(),
+        s: date.getSeconds()
+      }
     }
   }
   function DatePicker (options) {
     this.config = _.assign(defConfig, options)
-    this.maxId = new Date().getTime() // 输入框唯一标识
+    this.create = _.newDateObj()
+    this.maxId = this.create.date.getTime() // 输入框唯一标识
     this.init()
   }
   DatePicker.prototype = {
@@ -176,4 +267,5 @@
     }
   }
   win.DatePicker = DatePicker
+  console.log("Author: Jhin \r\nGithub: https://github.com/Jhinsama/DatePicker")
 })
